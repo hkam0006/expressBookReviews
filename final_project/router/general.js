2c4ebfe3e89bd0c76647db4e947f171e61a6123e
Hiu Long Kam (Sonny) <sonnykam18@gmail.com>
2024-08-16 01:53:41 +1000

    Completed task 1-3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,7 +4,6 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-
 public_users.post("/register", (req,res) => {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
@@ -12,20 +11,30 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(200).send(JSON.stringify(books));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  if (books[isbn]){
+    res.status(200).send(JSON.stringify(books[isbn]))
+  } else {
+    res.status(400).send(`Book with ${isbn} does not exist`);
+  }
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const result = {}
+  for (const key in books){
+    if (books[key].author === author){
+      result[key] = books[key]
+    }
+  }
+  res.status(200).send(JSON.stringify(result))
 });
 
 // Get all books based on title
